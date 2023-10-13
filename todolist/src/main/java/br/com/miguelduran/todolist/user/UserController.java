b84e50e46165beb970c44ded14aefcc61c79a648
Miguel Henrique Duran <miguelhdp009@gmail.com>
2023-10-13 16:45:23 -0300

    Dockerfile deploy

--- todolist/src/main/java/br/com/miguelduran/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/miguelduran/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.miguelduran.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
+    @Autowired
+    private IUserRepository userRepository;
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        UserModel user = this.userRepository.findByUsername(userModel.getUsername());
+        if (user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        String passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashed);
+
+        var userCreated = userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
